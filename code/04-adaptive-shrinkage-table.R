4ebe5955d5697175b0c03c6d5093db763646580d
Ioannis Kosmidis <ioannis.kosmidis@warwick.ac.uk>
2025-11-06 11:14:23 +0000

    R script for Table 1

---
+++ code/04-adaptive-shrinkage-table.R
@@ -0,0 +1,75 @@
+supp_path <- "."
+figures_path <- file.path(supp_path, "figures")
+results_path <- file.path(supp_path, "results/new")
+out_file <- file.path(results_path, "rescaled-mDYPL-estimates.rda")
+n_cores <- 10
+
+library("dplyr")
+library("ggplot2")
+library("ggpp")
+library("patchwork")
+library("parallel")
+## library("brglm2")
+devtools::load_all("~/Repositories/brglm2")
+
+source(file.path(supp_path, "code/methods/plot-with-insets.R"))
+source(file.path(supp_path, "code/methods/compute-pt.R"))
+source(file.path(supp_path, "code/methods/generate-unique-seeds.R"))
+
+## Estimates for setting 1
+estimate_s <- function(setting) {
+    n <- 1000
+    if (setting == "a") {
+        base_beta <- c(-3, -3/2, 0, 3/2, 3)
+        kappa <- 0.2
+        gamma <- sqrt(0.9)
+    } else if (setting == "b") {
+        base_beta <- c(-3, -3/2, 0, 3/2, 3)
+        kappa <- 0.05
+        gamma <- 10
+    } else if (setting == "c") {
+        base_beta <- c(-3, -3/2, 0, 3/2, 8)
+        kappa <- 0.2
+        gamma <- sqrt(3.1)
+    }
+    p <- n * kappa
+    beta0 <- rep(base_beta, p / 5)
+    beta0 <- sort(sqrt(n) * gamma * beta0 / sqrt(sum(beta0^2)))
+    X <- matrix(rnorm(n * p), nrow = n, ncol = p) / sqrt(n)
+    y <- rbinom(n, 1, plogis(drop(X %*% beta0)))
+    coefs <- glm(y ~ -1 + X, family = binomial(), method = "mdyplFit", alpha = 1 / (1 + kappa)) |> coef()
+    data.frame(estimate = coefs,
+               kappa = kappa,
+               gamma = gamma,
+               truth = beta0,
+               parameter = seq_along(beta0),
+               method = "mDYPL")
+}
+
+aggegates <- function(results) {
+    kappa <- unique(results$kappa)
+    gammasq <- unique(results$gamma)^2
+    biases <- with(results, c(tapply(estimate - truth, truth, mean), mean(estimate - truth)))
+    mses <- with(results, c(tapply((estimate - truth)^2, truth, mean), mean((estimate - truth)^2)))
+    out <- rbind(c(kappa, gammasq, biases),
+                 c(kappa, gammasq, sqrt(mses)))
+    colnames(out)[c(1:2, 8)] <- c("kappa", "gammasq", "Aggregate")
+    colnames(out)[3:7] <- round(as.numeric(colnames(out)[3:7]), 3)
+    rownames(out) <- c("bias", "rootMSE")
+    out
+}
+
+n_reps <- 5000
+set.seed(123)
+seeds <- generate_unique_seeds(n_reps)
+
+ests_sa <- mclapply(1:n_reps, function(i) { set.seed(seeds[i]); estimate_s("a") }, mc.cores = n_cores)
+ests_sb <- mclapply(1:n_reps, function(i) { set.seed(seeds[i]); estimate_s("b") }, mc.cores = n_cores)
+ests_sc <- mclapply(1:n_reps, function(i) { set.seed(seeds[i]); estimate_s("c") }, mc.cores = n_cores)
+ests_sa <- do.call("rbind", ests_sa)
+ests_sb <- do.call("rbind", ests_sb)
+ests_sc <- do.call("rbind", ests_sc)
+
+round(aggegates(ests_sa), 3)
+round(aggegates(ests_sb), 3)
+round(aggegates(ests_sc), 3)
