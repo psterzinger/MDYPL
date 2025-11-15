84e94eda8d722ce10d936aefaf7d2f759b37edc1
Ioannis Kosmidis <ioannis.kosmidis@warwick.ac.uk>
2025-11-15 23:15:58 +0000

    Intercept simulations in R

--- code/06-cLS-comparison.R
+++ code/06-cLS-comparison.R
@@ -1,17 +1,18 @@
 supp_path <- "."
 figures_path <- file.path(supp_path, "figures")
 results_path <- file.path(supp_path, "results/new")
-out_file <- file.path(results_path, "cLS-MDYPL-comparison-estimates.rda")
 n_cores <- 10
 
 ## library("brglm2")
 devtools::load_all("~/Repositories/brglm2")
+library("mvtnorm")
 library("dplyr")
 library("ggplot2")
 library("patchwork")
 library("parallel")
 library("glmnet")
 library("tictoc")
+
 
 source(file.path(supp_path, "code/methods/generate-unique-seeds.R"))
 
@@ -34,7 +35,7 @@ eta_lasso <- function(X, y) {
 simu_X <- function(n, kappa) {
     p <- floor(n * kappa)
     Sigma <- (matrix(1, p, p) + diag(p)) / 2
-    MASS::mvrnorm(n, rep(0, p), Sigma = Sigma)
+    rmvnorm(n, rep(0, p), sigma = Sigma)
 }
 
 simu_y <- function(X, beta) {
@@ -71,11 +72,9 @@ cls_z_stat <- function(estimate, beta0, X, eta, qrX = NULL) {
 
 mdypl <- function(X, y, alpha, start = NULL, se_start = c(0.5, 1, 1)) {
     fit <- glm(y ~ -1 + X, family = binomial(), method = "mdypl_fit", alpha = alpha, start = start)
-    ss <- try(summary(fit, hd_correction = TRUE,  se_start = se_start, init_iter = 0,
-                      se_solve_dots = list(ftol = 1e-12, xtol = 1e-12)), silent = TRUE)
+    ss <- try(summary(fit, hd_correction = TRUE,  se_start = se_start, init_iter = 0), silent = TRUE)
     if (inherits(ss, "try-error")) { # try another time
-        ss <- summary(fit, hd_correction = TRUE,  se_start = se_start * 0.9, init_iter = 0,
-                      se_solve_dots = list(ftol = 1e-12, xtol = 1e-12))
+        ss <- summary(fit, hd_correction = TRUE,  se_start = se_start * 0.9, init_iter = 0)
     }
     consts <- ss$se_parameters
     list(estimate = coef(ss)[, "Estimate"],
@@ -85,44 +84,46 @@ mdypl <- function(X, y, alpha, start = NULL, se_start = c(0.5, 1, 1)) {
          upsilon = sqrt(ss$signal_strength))
 }
 
+R <- 1000
+gamma <- 3
+alpha <- 0.95
+ns <- c(400, 800, 1200, 1600, 2000)
+kappas <- c(0.2, 0.5)
+nk <- expand.grid(n = ns, kappa = kappas)
+nk <- nk |>
+    mutate(path = file.path(results_path,
+                            paste0("cLS-MDYPL-comparison-estimates-", n, "-", kappa, ".rda")))
+nk$mu <- nk$b <- nk$sigma <- NA
+n_settings <- nrow(nk)
 
-if (file.exists(out_file)) {
-    load(out_file)
-} else {
-    R <- 1000
-    gamma <- 3
-    alpha <- 0.95
-    ns <- c(400, 800, 1200, 1600, 2000)
-    kappas <- c(0.2, 0.5)
-    nk <- expand.grid(n = ns, kappa = kappas)
-
-    ## Get oracle constants
-    nk$mu <- nk$b <- nk$sigma <- NA
-    n_settings <- nrow(nk)
-    se_init <- c(0.9, 2, 2)
-    for (i in 1:n_settings) {
-        consts <- solve_se(nk$kappa[i], gamma, alpha, start = se_init)
-        nk$mu[i] <- consts[1]
-        nk$b[i] <- consts[2]
-        nk$sigma[i] <- consts[3]
+if (all(file.exists(nk$path))) {
+    ## Gather results form images
+    all_estimates <- all_z_statistics <- all_constants <- NULL
+    for (path in nk$path) {
+        load(path)
+        all_estimates <- rbind(all_estimates, estimates)
+        all_z_statistics <- rbind(all_z_statistics, z_statistics)
+        all_constants <- rbind(all_constants, constants)
     }
-    nk <- nk |> mutate(root_aMSE = sqrt(2 / (n + 1 / kappa)) * sigma / mu)
-
+} else {
+    ## General unique seeds
     set.seed(123)
     seeds <- generate_unique_seeds(R * n_settings)
     seeds <- matrix(seeds, ncol = R)
-
     methds <- c("CLS", "CLS [O]", "rescaled MDYPL", "rescaled MDYPL [O]")
     n_methds <- length(methds)
     stats <-  c("CLS", "CLS [O]", "rescaled MDYPL", "rescaled MDYPL [O]")
     n_stats <- length(stats)
-    estimates <- z_statistics <- constants <- NULL
     for (i in 1:n_settings) {
         n <- nk[i, "n"]
         kappa <- nk[i, "kappa"]
-        b <- nk[i, "b"]
-        mu <- nk[i, "mu"]
-        sigma <- nk[i, "sigma"]
+        ## Get oracle constants and root MSE
+        se_init <- c(0.9, 2, 2)
+        consts <- solve_se(nk$kappa[i], gamma, alpha, start = se_init)
+        nk$mu[i] <- mu <- consts[1]
+        nk$b[i] <- b <- consts[2]
+        nk$sigma[i] <- sigma <- consts[3]
+        nk$root_aMSE[i] <- sqrt(2 / (n + 1 / kappa)) * sigma / mu
         cat("\nSetting", i, ": n =", n, " kappa=", kappa, "\n")
         set.seed(100)
         X <- simu_X(n, kappa)
@@ -182,58 +183,53 @@ if (file.exists(out_file)) {
             data.frame(mu = co[1], b = co[2], sigma = co[3], linf = max(abs(attr(co, "funcs"))),
                        uspilon = x$upsilon, gamma = gamma, kappa = kappa, N = n, p = p)
         })
-        constants <- rbind(constants,
-                           do.call("rbind", consts) |> mutate(sample = 1:R))
-        estimates <- rbind(estimates,
-                           data.frame(estimate = c(beta_cls, beta_cls_oracle, beta_mdypl, beta_mdypl_oracle),
-                                      method = rep(methds, each = R * p),
-                                      truth = beta_oracle,
-                                      parameter = rep(1:p, R * n_methds),
-                                      sample = rep(rep(1:R, each = p), n_methds),
-                                      kappa = kappa, gamma = gamma, N = n, p = p))
-        z_statistics <- rbind(z_statistics,
-                              data.frame(z = c(z_cls, z_cls_oracle, z_mdypl, z_mdypl_oracle),
-                                         statistic = rep(stats, each = R * p),
-                                         truth = beta_oracle,
-                                         parameter = rep(1:p, R * n_stats),
-                                         sample = rep(rep(1:R, each = p), n_stats),
-                                         kappa = kappa, gamma = gamma, N = n, p = p))
+        constants <- do.call("rbind", consts) |> mutate(sample = 1:R)
+        estimates <- data.frame(estimate = c(beta_cls, beta_cls_oracle, beta_mdypl, beta_mdypl_oracle),
+                                method = rep(methds, each = R * p),
+                                truth = beta_oracle,
+                                parameter = rep(1:p, R * n_methds),
+                                sample = rep(rep(1:R, each = p), n_methds),
+                                kappa = kappa, gamma = gamma, N = n, p = p)
+        z_statistics <- data.frame(z = c(z_cls, z_cls_oracle, z_mdypl, z_mdypl_oracle),
+                                   statistic = rep(stats, each = R * p),
+                                   truth = beta_oracle,
+                                   parameter = rep(1:p, R * n_stats),
+                                   sample = rep(rep(1:R, each = p), n_stats),
+                                   kappa = kappa, gamma = gamma, N = n, p = p)
         toc()
+        save(nk, constants, estimates, z_statistics, file = nk$path[i])
     }
-
-    methods_ordered <- c("CLS [O]", "CLS", "rescaled MDYPL [O]", "rescaled MDYPL", "MDYPL")
-    statistics_ordered <- c("CLS [O]", "CLS", "rescaled MDYPL [O]", "rescaled MDYPL")
-    z_statistics <- z_statistics |>
-        mutate(statistic = factor(statistic, levels = statistics_ordered, ordered = TRUE))
-    estimates <- estimates |>
-        mutate(method = factor(method, levels = methods_ordered, ordered = TRUE))
-
-    save(nk, constants, estimates, z_statistics, constants, file = out_file)
 }
 
-root_aMSE <- estimates |>
+methods_ordered <- c("CLS [O]", "CLS", "rescaled MDYPL [O]", "rescaled MDYPL", "MDYPL")
+statistics_ordered <- c("CLS [O]", "CLS", "rescaled MDYPL [O]", "rescaled MDYPL")
+all_z_statistics <- all_z_statistics |>
+    mutate(statistic = factor(statistic, levels = statistics_ordered, ordered = TRUE))
+all_estimates <- all_estimates |>
+    mutate(method = factor(method, levels = methods_ordered, ordered = TRUE))
+
+root_aMSE <- all_estimates |>
     group_by(method, sample, N, kappa) |> summarize(root_aMSEs = sqrt(mean((estimate - truth)^2))) |>
     group_by(method, N, kappa) |> summarize(root_aMSE = mean(root_aMSEs),
                                             sd = sd(root_aMSEs) / sqrt(n()),
                                             R = n())
-aBias <- estimates |>
+aBias <- all_estimates |>
     group_by(method, sample, N, kappa) |> summarize(aBiases = mean(estimate - truth)) |>
     group_by(method, N, kappa) |> summarize(aBias = mean(aBiases),
                                             sd = sd(aBiases) / sqrt(n()),
                                             R = n())
-aBias_zero <- estimates |> filter(truth == 0) |>
+aBias_zero <- all_estimates |> filter(truth == 0) |>
     group_by(method, sample, N, kappa) |> summarize(aBiases = mean(estimate - truth)) |>
     group_by(method, N, kappa) |> summarize(aBias = mean(aBiases),
                                             sd = sd(aBiases) / sqrt(n()),
                                             R = n())
-aBias_nonzero <- estimates |> filter(truth != 0) |>
+aBias_nonzero <- all_estimates |> filter(truth != 0) |>
     group_by(method, sample, N, kappa) |> summarize(aBiases = mean(estimate - truth)) |>
     group_by(method, N, kappa) |> summarize(aBias = mean(aBiases),
                                             sd = sd(aBiases) / sqrt(n()),
                                             R = n())
-Bias <- estimates |>
+Bias <- all_estimates |>
     group_by(method, parameter, N, kappa) |> summarize(bias = mean(estimate - truth))
-
 
 cols0 <- hcl.colors(3, alpha = 0.5)
 cols1 <- hcl.colors(3, alpha = 1)
@@ -289,13 +285,14 @@ p_aBias_zero <- ggplot(aBias_zero |> subset(method != "MDYPL")) +
 trans <- hcl.colors(1, alpha = 0)
 which_par <- 6:30
 which_N <- c(400, 800, 2000)
-p_pvalue <- ggplot(z_statistics |> subset(parameter %in% which_par & N %in% which_N)) +
+p_pvalue <- ggplot(all_z_statistics |> subset(parameter %in% which_par & N %in% which_N)) +
     geom_hline(aes(yintercept = 1), col = "grey") +
     geom_histogram(aes(2 * pnorm(-abs(z)), after_stat(density), col = statistic, fill = statistic),
                    breaks = seq(0, 1, by = 0.05)) +
     scale_x_continuous(limits = c(-0.5, 1.5)) +
     scale_color_manual(values = c(cols[2], cols[2], cols[4], cols[4])) +
     scale_fill_manual(values = c(trans, cols[1], trans, cols[3])) +
+    lims(y = c(0, 1.3)) +
     facet_grid(kappa + N ~ statistic, labeller = label_bquote(atop(kappa == .(kappa), N == .(N)))) +
     theme_minimal() +
     labs(x = expression(2 * Phi(-"|"~z~"|"))) +
---
+++ code/08-intercept-simu-quantile-tables.R
@@ -0,0 +1,116 @@
+supp_path <- "."
+figures_path <- file.path(supp_path, "figures")
+results_path <- file.path(supp_path, "results/new")
+out_file <- file.path(results_path, "intercept-simu-statistics.rda")
+n_cores <- 10
+
+## library("brglm2")
+devtools::load_all("~/Repositories/brglm2")
+library("dplyr")
+library("tidyr")
+library("parallel")
+library("RcppNumerical")
+library("tictoc")
+
+
+source(file.path(supp_path, "code/methods/generate-unique-seeds.R"))
+
+scaled_beta <- function(beta, n, kappa, gamma) {
+    p <- floor(n * kappa)
+    gamma * beta / sqrt(sum(beta^2) / n)
+}
+
+simu_X <- function(n, kappa) {
+    p <- floor(n * kappa)
+    matrix(rnorm(n * p, 0, 1 / sqrt(n)), n, p)
+}
+
+simu_y <- function(X, beta) {
+    eta <- drop(X %*% beta)
+    rbinom(length(eta), 1, plogis(eta))
+}
+
+fit_mdypl <- function(X, y, alpha = 1, start = rep(0, ncol(X)),
+                      eps_f = 1e-14, eps_g = 1e-14, maxit = 1000) {
+    ya <- alpha * y + (1 - alpha) / 2
+    res <- fastLR(X, ya, start, eps_f, eps_g, maxit = maxit)
+    res$pl <- sum(ya * log(fitted(res)) + (1 - ya) * log(1 - fitted(res)))
+    res
+}
+
+long2wide <- function(tab) {
+    res <- rbind(
+        pivot_wider(tab, names_from = "quantile", id_cols = c("kappa", "theta0"),
+                    values_from = "z_z"),
+        pivot_wider(tab, names_from = "quantile", id_cols = c("kappa", "theta0"),
+                    values_from = "z_nz"),
+        pivot_wider(tab, names_from = "quantile", id_cols = c("kappa", "theta0"),
+                    values_from = "plr_z"))
+    res$statistic <- rep(c("z_z", "z_nz", "plr_z"), each = nrow(res) / 3)
+    res
+}
+
+if (file.exists(out_file)) {
+    load(out_file)
+} else {
+    R <- 5000
+    gamma <- sqrt(5)
+    n <- 2000
+    kt <- expand.grid(theta0 = seq(0.5, 2.5, by = 1),
+                      kappa = seq(0.2, 0.8, by = 0.2)) |>
+        mutate(p = floor(n * kappa),
+               alpha = 1 / (1 + kappa))
+    n_settings <- nrow(kt)
+    quant <- c(1, 5, 10, 25, 50, 75, 90, 95, 99)
+    statistics <- NULL
+    set.seed(123)
+    seeds <- generate_unique_seeds(R * n_settings, maxint = 100 * R * n)
+    seeds <- matrix(seeds, ncol = R)
+    for (i in 1:n_settings) {
+        theta0 <- kt[i, "theta0"]
+        kappa <- kt[i, "kappa"]
+        alpha <- kt[i, "alpha"]
+        cat(i, "/", n_settings, ": Computing statistics for kappa =", kappa, "and theta0 =", theta0, "\n")
+        tic()
+        p <- kt[i, "p"]
+        tau = 1 / sqrt(n)
+        beta0 <- rep(0, p)
+        beta0[sample(1:p, p/4, replace = FALSE)] <- 1
+        beta0 <- scaled_beta(beta0, n, kappa, gamma)
+        inds_z <- which(beta0 == 0)[1:10]
+        coord_z <- inds_z[1]
+        coord_nz <- which(beta0 != 0)[1]
+        se_consts <- solve_se(kappa, gamma, alpha, intercept = theta0, corrupted = FALSE, start = c(0.5, 1, 1, theta0))
+        results <- mclapply(1:R, function(r) {
+            if (r %% 100 == 0) cat(r, "/", R, "\n")
+            set.seed(seeds[i, r])
+            X_full <- simu_X(n, kappa)
+            X_nest <- X_full[, -inds_z]
+            X_full <- cbind(1, X_full)
+            X_nest <- cbind(1, X_nest)
+            y <- simu_y(X_full, c(theta0, beta0))
+            start_full <- c(se_consts[4], beta0 * se_consts[1])
+            start_nest <- start_full[-c(inds_z + 1)]
+            m_full <- fit_mdypl(X_full, y, alpha, start_full, 1e-14, 1e-14, 1000)
+            m_nest <- fit_mdypl(X_nest, y, alpha, start_nest, 1e-14, 1e-14, 1000)
+            plr_z <- 2 * (m_full$pl - m_nest$pl) * se_consts[2] / (kappa * se_consts[3]^2)
+            z_z <- sqrt(n) * tau * coef(m_full)[coord_z + 1] / se_consts[3]
+            z_nz <- sqrt(n) * tau * (coef(m_full)[coord_nz + 1] - se_consts[1] * beta0[coord_nz]) / se_consts[3]
+            data.frame(z_z = z_z, z_nz = z_nz, plr_z = plr_z)
+        }, mc.cores = n_cores)
+        toc()
+        results <- do.call("rbind", results)
+        tab <- results |>
+            reframe(z_z = sapply(qnorm(quant / 100), function(q) 100 * mean(z_z < q)),
+                    z_nz = sapply(qnorm(quant / 100), function(q) 100 * mean(z_nz < q)),
+                    plr_z = sapply(qchisq(quant / 100, length(inds_z)), function(q) 100 * mean(plr_z < q))) |>
+            mutate(quantile = quant, kappa = kappa, theta0 = theta0)
+        statistics <- rbind(statistics, tab)
+        print(long2wide(tab))
+    }
+    save(statistics, file = out_file)
+}
+
+long2wide(statistics) |>
+    filter(statistic == "plr_z") # |>
+    # data.frame() |> memisc:::toLatex.data.frame(digits = 1)
